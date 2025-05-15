2cebe3c3e6552a288d04c394ce4b3e3fb78dae7b
hongyerin <yerinhong00@gmail.com>
2025-05-15 10:39:17 +0900

    ⚰️ cleanup: 주석 정리 #2

--- src/typescript/vote.ts
+++ src/typescript/vote.ts
@@ -44,28 +44,3 @@ profiles.forEach(profile => {
         votedTargetUserId = targetUserId;
     });
 });
-
-// // 모든 프로필 요소 가져오기
-// // HTMLElement : 타입 단언, ts에서 DOM요소로 인식시킴
-// const profiles = document.querySelectorAll<HTMLElement>('.profile');
-
-// //가져온 모든 프로필에 대해 하나씩 반복문
-// profiles.forEach(profile => {
-//     profile.addEventListener('click', () => {
-//         const voteIcon = profile.querySelector('.vote-icon'); //클릭한 profile 안에서 class="vote-icon"인 자식 요소 찾음. 투표 이미지가 있는 레이어임
-//         if (!voteIcon) return; //만약 voteIcon 없으면 함수 실행 중단
-
-//         // 토글 : 숨겨신 상태면 보이게, 보이는 상태면 숨기기/ 클릭할때마다 표시 숨김 반복
-//         // voteItem.classList.toggle('hidden');
-
-//         // hidden 상태면 hidden을 제거하고 flex를 추가
-//         // flex와 hidden이 충돌나서 변경함
-//         if (voteIcon.classList.contains('hidden')) {
-//             voteIcon.classList.remove('hidden');
-//             voteIcon.classList.add('flex');
-//         } else {
-//             voteIcon.classList.add('hidden');
-//             voteIcon.classList.remove('flex');
-//         }
-//     });
-// });

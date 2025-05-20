058dd56eab7affc9c78bfb7acaccc01595268191
jinseob92 <elso1234@naver.com>
2025-05-20 17:48:17 +0900

    ✨feat: 마피아 기능 구현(전체 유저에 알람 기능)

--- src/js/chat/kill.ts
+++ src/js/chat/kill.ts
@@ -1,37 +1,10 @@
-import { socket } from '../lib/yongchat';
+import { sendMsg } from '../lib/yongchat';
 
-export function renderKillTargets(
-    members: { user_id: string; nickName: string }[],
-    myUserId: string,
-    myRole: string,
-) {
-    if (myRole !== 'mafia') {
-        return;
-    }
-
-    const container = document.querySelector('#kill-target') as HTMLElement;
-    container.innerHTML = '<h3>킬 대상 선택:</h3>';
-
-    members.forEach(member => {
-        if (member.user_id === myUserId) {
-            // 자기 자신은 제외 시킴
-            return;
-        }
-        const btn = document.createElement('button');
-        btn.textContent = member.nickName;
-        btn.className = 'kill-button';
-        btn.addEventListener('click', () => {
-            const confirmKill = confirm(`${member.nickName}을 죽이시겠습니까?`);
-            if (confirmKill) {
-                socket.emit('message', {
-                    action: 'kill',
-                    from: myUserId,
-                    targetId: member.user_id,
-                });
-                console.log(`${member.nickName}을 죽였습니다.`);
-                container.innerHTML = `<p>선택 완료</p>`;
-            }
-        });
-        container.appendChild(btn);
+export function mafiaKill(user_id: string, targetId: string) {
+    console.log(`${user_id}가  ${targetId}을 죽임`);
+    sendMsg({
+        action: 'kill',
+        targetId: targetId,
+        from: user_id,
     });
 }
